400a3f2216a0eceba340745c500b7064a8d11779
James Clifford <engineer@jclifford.ie>
2018-01-13 11:54:47 +0000

    views5

---
+++ app/overrides/spree/users/show.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path 			=> "spree/users/show",
+                     :name         			=> "add_favorites_to_user",
+                     :insert_before 		=> "[data-hook='account_my_orders']",
+                     :partial      			=> 'spree/favorites/product_favorites')
