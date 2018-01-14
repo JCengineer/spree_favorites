0bafa4361430b5bd93c5a2911d53d09bec204a25
James Clifford <engineer@jclifford.ie>
2018-01-14 16:20:35 +0000

    view icons

---
+++ app/overrides/spree/products/show.rb
@@ -0,0 +1,11 @@
+Deface::Override.new(:virtual_path 			=> "spree/products/show",
+                     :name         			=> "add_favorites_link_to_product",
+                     :insert_before 		=> "div[itemprop='description']",
+                     :partial      			=> 'spree/products/add_favorite_link')
+
+
+Deface::Override.new(:virtual_path 			=> "spree/products/show",
+                     :name         			=> "add_favorites_icon_to_product",
+                     :insert_before 		=> "main-image .panel-body",
+                     :partial      			=> 'spree/products/favourite_icon')
+
