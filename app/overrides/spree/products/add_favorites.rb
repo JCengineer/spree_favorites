39e18fd97e5c8af9b7540cc49cb7119f6ceef70a
James Clifford <engineer@jclifford.ie>
2018-01-14 21:01:49 +0000

    views

--- app/overrides/spree/products/add_favorites.rb
+++ app/overrides/spree/products/add_favorites.rb
@@ -1,17 +1,17 @@
 Deface::Override.new(:virtual_path 			=> "spree/products/show",
                      :name         			=> "add_favorites_link_to_product",
                      :insert_before 		=> "div[itemprop='description']",
-                     :partial      			=> 'spree/products/add_favorite_link')
+                     :partial      			=> 'spree/shared/add_favorite_link')
 
 
 Deface::Override.new(:virtual_path 			=> "spree/products/show",
                      :name         			=> "add_favorites_icon_to_product",
                      :insert_bottom 		=> "#main-image .panel-body",
-                     :partial      			=> 'spree/products/favorite_icon')
+                     :partial      			=> 'spree/shared/favorite_icon')
 
 
 Deface::Override.new(:virtual_path 			=> "spree/products/_product_item",
                      :name         			=> "add_favorites_icon_to_product_item",
                      :insert_after  		=> "erb[loud]:contains('product_image')",
-                     :partial      			=> 'spree/products/favorite_icon_item')
+                     :partial      			=> 'spree/shared/favorite_icon_item')
 
---
+++ app/overrides/spree/suppliers/add_favorites.rb
@@ -0,0 +1,17 @@
+Deface::Override.new(:virtual_path 			=> "spree/suppliers/show",
+                     :name         			=> "add_favorites_link_to_supplier",
+                     :insert_before 		=> "#supplier-description",
+                     :partial      			=> 'spree/shared/add_favorite_link')
+
+
+Deface::Override.new(:virtual_path 			=> "spree/suppliers/show",
+                     :name         			=> "add_favorites_icon_to_supplier",
+                     :insert_before 		=> "#supplier-profile-picture",
+                     :partial      			=> 'spree/shared/favorite_icon')
+
+
+Deface::Override.new(:virtual_path 			=> "spree/suppliers/_supplier",
+                     :name         			=> "add_favorites_icon_to_supplier_item",
+                     :insert_after  		=> "erb[loud]:contains('supplier.profile_picture')",
+                     :partial      			=> 'spree/shared/favorite_icon_item')
+
