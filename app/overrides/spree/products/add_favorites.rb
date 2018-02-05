e5f2d9badd7860e181a6bde641ef89d78bd5af3b
James Clifford <engineer@jclifford.ie>
2018-02-05 22:27:19 +0000

    locals string

--- app/overrides/spree/products/add_favorites.rb
+++ app/overrides/spree/products/add_favorites.rb
@@ -7,11 +7,11 @@ Deface::Override.new(:virtual_path 			=> "spree/products/show",
 Deface::Override.new(:virtual_path 			=> "spree/products/show",
                      :name         			=> "add_favorites_icon_to_product",
                      :insert_bottom 		=> "#main-image .panel-body",
-                     :text      				=> '<%= render partial: spree/shared/favorite_icon, locals: {item: product} %>')
+                     :text      				=> '<%= render partial: "spree/shared/favorite_icon", locals: {item: product} %>')
 
 
 Deface::Override.new(:virtual_path 			=> "spree/products/_product_item",
                      :name         			=> "add_favorites_icon_to_product_item",
                      :insert_after  		=> "erb[loud]:contains('product_image')",
-                     :text      				=> '<%= render partial: spree/shared/favorite_icon, locals: {item: @product} %>')
+                     :text      				=> '<%= render partial: "spree/shared/favorite_icon", locals: {item: @product} %>')
 
