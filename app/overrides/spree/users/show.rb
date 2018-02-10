59e3dc60aeacb47c40c1fb57d8e704ecb7c73b54
James Clifford <engineer@jclifford.ie>
2018-02-10 17:53:59 +0000

    lenders on account page

--- app/overrides/spree/users/show.rb
+++ app/overrides/spree/users/show.rb
@@ -2,3 +2,8 @@ Deface::Override.new(:virtual_path 			=> "spree/users/show",
                      :name         			=> "add_favorites_to_user",
                      :insert_before 		=> "[data-hook='account_my_orders']",
                      :partial      			=> 'spree/favorites/product_favorites')
+
+Deface::Override.new(:virtual_path 			=> "spree/users/show",
+                     :name         			=> "add_favorites_to_user",
+                     :insert_before 		=> "[data-hook='account_my_orders']",
+                     :partial      			=> 'spree/favorites/supplier_favorites')
